7891d2cf7d7b3ccf0ef5761292ca73019b5e6b5a
SANJAYKUMAR K <117352009+SANJAYKUMAR-GITHUB@users.noreply.github.com>
2025-02-28 17:59:04 +0530

    Add files via upload

---
+++ React/react-app1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Class_comp_hooks'; // Import the Counter component
+
+function App() {
+  return (
+    <div>
+      <h1>React Counter App</h1>
+      <Counter /> {/* Render the Counter component */}
+    </div>
+  );
+}
+
+export default App;
