2c6da1c196e291f89e409af14d6509e0839bde47
SANJAYKUMAR K <117352009+SANJAYKUMAR-GITHUB@users.noreply.github.com>
2025-02-28 17:10:50 +0530

    Add files via upload

---
+++ C#/Arithmetic_ope/Arithmetic_ope/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arithmetic_ope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int nums2;
+            int result;
+
+            Console.WriteLine("enter first number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            nums2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add is: " + (num1 + nums2));
+            Console.WriteLine("sub is: " + (num1 - nums2));
+            Console.WriteLine("mul is: " + (num1 * nums2));
+            Console.WriteLine("div is: " + (num1 / nums2));
+            Console.WriteLine("mod is: " + (num1 % nums2));
+
+            Console.ReadLine();
+        }
+    }
+}
